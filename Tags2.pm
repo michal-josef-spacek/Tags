c86813903cbbc1c08b03793bbe6da67a3f6fa3ee
Michal Špaček <skim@cpan.org>
2005-07-14 21:37:35 +0000

    First version.

---
+++ Tags2.pm
@@ -0,0 +1,217 @@
+#------------------------------------------------------------------------------
+package Tags2;
+#------------------------------------------------------------------------------
+# $Id: Tags2.pm,v 1.1 2005-07-14 21:37:35 skim Exp $
+
+# Pragmas.
+use strict;
+
+# Modules.
+use Carp;
+
+# Version.
+our $VERSION = 0.1;
+
+#------------------------------------------------------------------------------
+sub new {
+#------------------------------------------------------------------------------
+# Constructor.
+
+	my $class = shift;
+	my $self = bless {}, $class;
+
+	# Set output handler.
+	$self->{'output_handler'} = '';
+
+	# Process params.
+        while (@_) {
+                my $key = shift;
+                my $val = shift;
+                croak "Bad parameter '$key'." if ! exists $self->{$key};
+                $self->{$key} = $val;
+        }
+
+	# Flush code.
+	$self->{'flush_code'} = '';
+
+	# Tmp code.
+	$self->{'tmp_code'} = [];
+
+	# Printed tags.
+	$self->{'printed_tags'} = [];
+
+	# Class.
+	$self->{'class'} = $class;
+
+	# Object.
+	return $self;
+}
+
+#------------------------------------------------------------------------------
+sub flush {
+#------------------------------------------------------------------------------
+# Flush tags in object.
+
+	my $self = shift;
+	if ($self->{'output_handler'}) {
+		my $ouf = $self->{'output_handler'};
+		print $ouf $self->{'flush_code'};
+	} else {
+		return $self->{'flush_code'};
+	}
+}
+
+#------------------------------------------------------------------------------
+sub open_tags {
+#------------------------------------------------------------------------------
+# Return array of opened tags.
+
+	my $self = shift;
+	return @{$self->{'printed_tags'}};
+}
+
+#------------------------------------------------------------------------------
+sub finalize {
+#------------------------------------------------------------------------------
+# Finalize Tags output.
+
+	my $self = shift;
+	while ($#{$self->{'printed_tags'}} != -1) {
+		$self->put(['e', shift @{$self->{'printed_tags'}}]);
+	}
+}
+
+#------------------------------------------------------------------------------
+sub put {
+#------------------------------------------------------------------------------
+# Put tags code.
+
+	my $self = shift;
+	my @data = @_;
+
+	# For every data.
+	foreach my $dat (@data) {
+
+		# Bad data.
+		unless (ref $dat eq 'ARRAY') {
+			croak "$self->{'class'}: Bad data.";
+		}
+
+		$self->_detect_data($dat);
+	}
+}
+
+#------------------------------------------------------------------------------
+sub reset {
+#------------------------------------------------------------------------------
+# Resets internal variables.
+
+	my $self = shift;
+	$self->{'printed_tags'} = [];
+	$self->{'flush_code'} = '';
+}
+
+#------------------------------------------------------------------------------
+# Private methods.
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+sub _detect_data {
+#------------------------------------------------------------------------------
+# Detect data.
+
+	my $self = shift;
+	my $data = shift;
+
+	# Attributes.
+	if ($data->[0] eq 'a') {
+		unless ($#{$self->{'tmp_code'}} > -1) {
+			croak "Bad tag type 'a'.";
+		}
+		shift @{$data};
+		while (@{$data}) {
+			my $par = shift @{$data};
+			my $val = shift @{$data};
+			push @{$self->{'tmp_code'}}, " $par=\"$val\"";
+		}
+
+	# Begin of tag.
+	} elsif ($data->[0] eq 'b') {
+		if ($#{$self->{'tmp_code'}} > -1) {
+			$self->_flush_tmp('>');
+		}
+		push @{$self->{'tmp_code'}}, "<$data->[1]";
+		push @{$self->{'printed_tags'}}, $data->[1];
+
+	# Comment.
+	} elsif ($data->[0] eq 'c') {
+		if ($#{$self->{'tmp_code'}} > -1) {
+			$self->_flush_tmp('>');
+		}
+		shift @{$data};
+		$self->{'flush_code'} .= '<!--';
+		foreach my $d (@{$data}) {
+			$self->{'flush_code'} .= ' ';
+			$self->{'flush_code'} .= ref $d eq 'SCALAR' ? ${$d} 
+				: $d;
+		}
+		$self->{'flush_code'} .= ' -->';
+
+	# Data.
+	} elsif ($data->[0] eq 'd') {
+		if ($#{$self->{'tmp_code'}} > -1) {
+			$self->_flush_tmp('>');
+		}
+		shift @{$data};
+		foreach my $d (@{$data}) {
+			$self->{'flush_code'} .= ' ';
+			$self->{'flush_code'} .= ref $d eq 'SCALAR' ? ${$d} 
+				: $d;
+		}
+
+	# End of tag.
+	} elsif ($data->[0] eq 'e') {
+		my $printed = shift @{$self->{'printed_tags'}};
+		unless ($printed eq $data->[1]) {
+			croak "$self->{'class'}: Ending bad tag: ".
+				"'$data->[1]' in block of tag '$printed'.";
+		}
+		if ($#{$self->{'tmp_code'}} > -1) {
+			$self->_flush_tmp(' />');
+		} else {
+			$self->{'flush_code'} .= "</$data->[1]>";
+		}
+
+	# Instruction.
+	} elsif ($data->[0] eq 'i') {
+		if ($#{$self->{'tmp_code'}} > -1) {
+			$self->_flush_tmp('>');
+		}
+		shift @{$data};
+		$self->{'flush_code'} .= '<?';
+		while (@{$data}) {
+			my $target = shift @{$data};
+			my $data = shift @{$data};
+			$self->{'flush_code'} .= " $target=$data";
+		}
+		$self->{'flush_code'} .= ' ?>';
+
+	# Other.
+	} else {
+		croak "$self->{'class'}: Bad type of data.";
+	}
+}
+
+#------------------------------------------------------------------------------
+sub _flush_tmp {
+#------------------------------------------------------------------------------
+# Flush $self->{'tmp_code'}.
+
+	my $self = shift;
+	my $string = shift;
+	push @{$self->{'tmp_code'}}, $string if $string;
+	$self->{'flush_code'} .= join('', @{$self->{'tmp_code'}});
+	$self->{'tmp_code'} = [];
+}
+
+1;
