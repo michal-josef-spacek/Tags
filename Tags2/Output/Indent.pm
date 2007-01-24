32bf13154cfe926d3515541fa803fe4482c35db6
Michal Špaček <skim@cpan.org>
2007-01-24 12:58:18 +0000

    New version.

---
+++ Tags2/Output/Indent.pm
@@ -0,0 +1,43 @@
+#------------------------------------------------------------------------------
+package Tags2::Output::Indent;
+#------------------------------------------------------------------------------
+# $Id: Indent.pm,v 1.2 2007-01-24 12:59:21 skim Exp $
+
+# Pragmas.
+use strict;
+
+# Modules.
+use Error::Simple::Multiple;
+
+# Version.
+our $VERSION = 0.01;
+
+#------------------------------------------------------------------------------
+sub new {
+#------------------------------------------------------------------------------
+# Constructor.
+
+	my $class = shift;
+	my $self = bless {}, $class;
+
+	# Set indent.
+	$self->{'set_indent'} = 0;
+
+	# Indent params.
+	$self->{'next_indent'} = '  ';
+	$self->{'line_size'} = 79;
+	$self->{'linebreak'} = "\n";
+
+	# Process params.
+        while (@_) {
+                my $key = shift;
+                my $val = shift;
+                err "Bad parameter '$key'." if ! exists $self->{$key};
+                $self->{$key} = $val;
+        }
+
+	# Object.
+	return $self;
+}
+
+1;
