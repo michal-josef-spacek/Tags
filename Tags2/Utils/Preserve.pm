e594bbcdad71b01cdc120a65601d41b4b28f18a5
Michal Špaček <skim@cpan.org>
2008-12-10 23:05:46 +0000

    Rewrite to better version (perlcritic).

--- Tags2/Utils/Preserve.pm
+++ Tags2/Utils/Preserve.pm
@@ -4,29 +4,35 @@ package Tags2::Utils::Preserve;
 
 # Pragmas.
 use strict;
+use warnings;
 
 # Modules.
 use Error::Simple::Multiple;
+use List::MoreUtils qw(any);
+use Readonly;
+
+# Constants.
+Readonly::Scalar my $LAST_INDEX => -1;
 
 # Version.
 our $VERSION = 0.01;
 
 #------------------------------------------------------------------------------
-sub new($@) {
+sub new {
 #------------------------------------------------------------------------------
 # Constructor.
 
-	my $class = shift;
+	my ($class, @params) = @_;
 	my $self = bless {}, $class;
 
 	# Preserved tags.
 	$self->{'preserved'} = [];
 
 	# Process params.
-        while (@_) {
-                my $key = shift;
-                my $val = shift;
-                err "Bad parameter '$key'." unless exists $self->{$key};
+        while (@params) {
+                my $key = shift @params;
+                my $val = shift @params;
+                err "Bad parameter '$key'." if ! exists $self->{$key};
                 $self->{$key} = $val;
         }
 
@@ -38,63 +44,67 @@ sub new($@) {
 }
 
 #------------------------------------------------------------------------------
-sub begin($$) {
+sub begin {
 #------------------------------------------------------------------------------
 # Process for begin of tag.
 
 	my ($self, $tag) = @_;
 	$self->save_previous;
-	if (grep { $tag eq $_ } @{$self->{'preserved'}}) {
+	if (scalar @{$self->{'preserved'}}
+		&& any { $tag eq $_ } @{$self->{'preserved'}}) {
+
 		push @{$self->{'preserved_stack'}}, $tag;
 		$self->{'preserved_flag'} = 1;
 	}
 
 	# Return preserved flag.
-	return wantarray ? ($self->{'preserved_flag'}, 
+	return wantarray ? ($self->{'preserved_flag'},
 		$self->{'prev_preserved_flag'}) : $self->{'preserved_flag'};
 }
 
 #------------------------------------------------------------------------------
-sub save_previous($) {
+sub save_previous {
 #------------------------------------------------------------------------------
 # Save previsou stay.
 
 	my $self = shift;
 	$self->{'prev_preserved_flag'} = $self->{'preserved_flag'};
+
+	return;
 }
 
 #------------------------------------------------------------------------------
-sub end($$) {
+sub end {
 #------------------------------------------------------------------------------
 # Process for end of tag.
 
 	my ($self, $tag) = @_;
 	$self->save_previous;
 	my $stack = $self->{'preserved_stack'};
-	if ($#{$stack} > -1 && $tag eq $stack->[$#{$stack}]) {
+	if (scalar @{$stack} && $tag eq $stack->[$LAST_INDEX]) {
 		pop @{$stack};
-		if ($#{$stack} == -1) {
+		if (! scalar @{$stack}) {
 			$self->{'preserved_flag'} = 0;
 		}
 	}
 
 	# Return preserved flag.
-	return wantarray ? ($self->{'preserved_flag'}, 
+	return wantarray ? ($self->{'preserved_flag'},
 		$self->{'prev_preserved_flag'}) : $self->{'preserved_flag'};
 }
 
 #------------------------------------------------------------------------------
-sub get($) {
+sub get {
 #------------------------------------------------------------------------------
 # Get preserved flag.
 
 	my $self = shift;
-	return wantarray ? ($self->{'preserved_flag'}, 
+	return wantarray ? ($self->{'preserved_flag'},
 		$self->{'prev_preserved_flag'}) : $self->{'preserved_flag'};
 }
 
 #------------------------------------------------------------------------------
-sub reset($) {
+sub reset {
 #------------------------------------------------------------------------------
 # Resets.
 
@@ -108,6 +118,8 @@ sub reset($) {
 
 	# Preserved tag.
 	$self->{'preserved_stack'} = [];
+
+	return;
 }
 
 1;
