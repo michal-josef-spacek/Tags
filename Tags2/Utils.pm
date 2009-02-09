b510aa58503fc9ec97333b80367609b299eb47f2
Michal Špaček <skim@cpan.org>
2009-02-09 11:36:57 +0000

    Implement encode_base_entities() subroutine.

--- Tags2/Utils.pm
+++ Tags2/Utils.pm
@@ -9,9 +9,10 @@ use warnings;
 
 # Modules.
 use Readonly;
+use HTML::Entities;
 
 # Constants.
-Readonly::Array our @EXPORT_OK => qw(encode_newline);
+Readonly::Array our @EXPORT_OK => qw(encode_newline encode_base_entities);
 
 # Version.
 our $VERSION = 0.01;
@@ -24,6 +25,20 @@ sub encode_newline {
 	my $string = shift;
 	$string =~ s/\n/\\n/gms;
 	return $string;
+}
+
+#------------------------------------------------------------------------------
+sub encode_base_entities {
+#------------------------------------------------------------------------------
+# Encode '<>&' base entities.
+
+	my $data = shift;
+	if (ref $data eq 'SCALAR') {
+		${$data} = encode_entities(${$data}, '<>&');
+		return;
+	} else {
+		return encode_entities($data, '<>&');
+	}
 }
 
 1;
