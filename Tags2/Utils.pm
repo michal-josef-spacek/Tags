7636ca1f6f1377849214f83a6a7780e8fce40ad5
Michal Špaček <skim@cpan.org>
2009-03-03 02:29:44 +0000

    Add TODO. Add doc stub. Rewrite $data to $data_r.

--- Tags2/Utils.pm
+++ Tags2/Utils.pm
@@ -31,18 +31,93 @@ sub encode_newline {
 sub encode_base_entities {
 #------------------------------------------------------------------------------
 # Encode '<>&' base entities.
+# TODO Other types.
 
-	my $data = shift;
-	if (ref $data eq 'SCALAR') {
-		${$data} = encode_entities(${$data}, '<>&');
+	my $data_r = shift;
+	if (ref $data_r eq 'SCALAR') {
+		${$data_r} = encode_entities(${$data_r}, '<>&');
 		return;
-	} elsif (ref $data eq 'ARRAY') {
-		foreach my $one_data (@{$data}) {
+	} elsif (ref $data_r eq 'ARRAY') {
+		foreach my $one_data (@{$data_r}) {
 			encode_base_entities(\$one_data);
 		}
 	} else {
-		return encode_entities($data, '<>&');
+		return encode_entities($data_r, '<>&');
 	}
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+ Tags2::Utils - Utils module for Tags2.
+
+=head1 SYNOPSIS
+
+ use Tags2::Utils qw(encode_newline encode_base_entities);
+ TODO
+
+=head1 SUBROUTINES
+
+=over 8
+
+=item B<encode_newline($string)>
+
+ TODO
+
+=item B<encode_base_entities($data_r)>
+
+ TODO
+
+=back
+
+=head1 ERRORS
+
+ No errors.
+
+=head1 EXAMPLE
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Tags2::Utils qw(TODO);
+
+ TODO
+
+=head1 DEPENDENCIES
+
+L<Readonly(3pm)>,
+L<HTML::Entities(3pm)>.
+
+=head1 SEE ALSO
+
+L<Tags2(3pm)>,
+L<Tags2::Output::Core(3pm)>,
+L<Tags2::Output::ESIS(3pm)>,
+L<Tags2::Output::Indent(3pm)>,
+L<Tags2::Output::LibXML(3pm)>,
+L<Tags2::Output::PYX(3pm)>,
+L<Tags2::Output::Raw(3pm)>,
+L<Tags2::Output::SESIS(3pm)>.
+
+=head1 AUTHOR
+
+ Michal Špaček L<tupinek@gmail.com>
+
+=head1 LICENSE AND COPYRIGHT
+
+ BSD license.
+
+=head1 VERSION
+
+ 0.01
+
+=cut
