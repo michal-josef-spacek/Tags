b635990adfb724aa5d5aab6d74c9553a3c1897e1
Michal Špaček <tupinek@gmail.com>
2011-05-17 21:45:43 +0200

    Change version to 0.01.

--- Tags/Output/Raw.pm
+++ Tags/Output/Raw.pm
@@ -18,7 +18,7 @@ Readonly::Scalar my $LAST_INDEX => -1;
 Readonly::Scalar my $SPACE => q{ };
 
 # Version.
-our $VERSION = 0.06;
+our $VERSION = 0.01;
 
 # Resets internal variables.
 sub reset {
@@ -603,6 +603,6 @@ BSD license.
 
 =head1 VERSION
 
-0.06
+0.01
 
 =cut
--- t/Tags::Output::Raw/01-version.t
+++ t/Tags::Output::Raw/01-version.t
@@ -3,4 +3,4 @@ use Tags::Output::Raw;
 use Test::More 'tests' => 1;
 
 # Test.
-is($Tags::Output::Raw::VERSION, '0.06');
+is($Tags::Output::Raw::VERSION, '0.01');
