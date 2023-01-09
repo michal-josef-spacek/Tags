1db6d0b5e3485caf13bdd2e8557632a73f8e68fa
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-01-09 11:29:11 +0100

    Prepare for 0.15 version

--- Tags/Utils.pm
+++ Tags/Utils.pm
@@ -15,7 +15,7 @@ Readonly::Scalar my $ATTR_CHARS => q{<&"};
 Readonly::Scalar my $CHAR_CHARS => q{<&\240};
 Readonly::Scalar my $EMPTY_STR => q{};
 
-our $VERSION = 0.14;
+our $VERSION = 0.15;
 
 # Encode newline in data to '\n' in output.
 sub encode_newline {
@@ -202,6 +202,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.14
+0.15
 
 =cut
